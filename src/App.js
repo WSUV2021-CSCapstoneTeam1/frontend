eee1bb1448c886b3dc78a268c29fd5868aa03306
Malcolm Anderson <malcolminyo@gmail.com>
2021-10-26 21:45:21 -0700

    Attempting to use react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
+import './App.css';
+import { Component } from 'react';
+
+class Welcome extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { stuff: "nothing loaded yet" };
+  }
+
+  componentDidMount() {
+    // Simple GET request using fetch
+    // fetch('http://127.0.0.1:8080/api/v1/factory/getAll')
+    //   .then(response => {
+    //     console.log(response);
+    //     console.log(response.text());
+    //   })
+    //   .then(data => {
+    //     console.log(data);
+    //     this.setState({ stuff: data });
+    //   });
+  }
+
+  render()
+  {
+    return (
+      <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+    );
+  }
+}
+
+function Home() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Home</h2>
+      <p>This is the home page.</p>
     </div>
   );
+}
+
+function Contact() {
+  return (
+    <div>
+      <h2>Contact</h2>
+      <p>This is the contact page.</p>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+      <p>This is a placeholder website.</p>
+    </div>
+  );
+}
+
+function App() {
+  return <Welcome/>
 }
 
 export default App;
