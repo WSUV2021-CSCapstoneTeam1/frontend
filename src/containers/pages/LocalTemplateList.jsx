e2a2d850a9fafdfa70a9d85f064e05eafb864854
Irina Bejan <irina.bejan@wsu.edu>
2021-12-01 18:09:17 -0800

    Add "Add Template" button to Local Templates page

--- src/containers/pages/LocalTemplateList.jsx
+++ src/containers/pages/LocalTemplateList.jsx
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import { Link } from "react-router-dom";
 
 class LocalTemplateList extends Component {
     /*
@@ -9,6 +10,11 @@ class LocalTemplateList extends Component {
         <div>
           <h2>Local Templates</h2>
           <p>These are the templates loaded from the backend database.</p>
+
+          {/* Add Button */}
+          <div className="mb-3">
+              <Link to="templates/add" className="btn btn-primary">Add Template</Link>
+          </div>
         </div>
       );
     }
