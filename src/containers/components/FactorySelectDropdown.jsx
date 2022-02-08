9889767749f819768e4e4dfc9d650fab4650b28a
Irina Bejan <irina.bejan@wsu.edu>
2022-02-08 13:33:15 -0800

    Call backend to populate dropdown with factory names

--- src/containers/components/FactorySelectDropdown.jsx
+++ src/containers/components/FactorySelectDropdown.jsx
@@ -13,6 +13,22 @@ class FactorySelectDropdown extends Component {
     componentDidMount() {
         // TODO: fetch the list of factories from the backend, and populate
         // the options available
+        const headers = { 'Content-Type': 'application/json', 'Accept': 'application/json' }
+            fetch('http://54.191.60.209:8090/BackendApi-1.0-SNAPSHOT/api/team/rds/get/all', headers)
+              .then(response => {
+                if (response.ok) {
+                   return response.json()
+               }
+               throw response;
+              })
+              .then(data => {
+                this.setState({
+                  allFactories: data
+                })
+              })
+              .catch(error => {
+                console.log(error);
+              })
     }
 
     onFactoryChanged(event) {
@@ -27,12 +43,23 @@ class FactorySelectDropdown extends Component {
     }
 
     render() {
+        var factories = [];
+        if (this.state.allFactories.data == null) {
+            console.log('no factories');
+            factories = null;
+        }
+        else {
+            console.log('we got factos');
+            factories = this.state.allFactories.data.map((item) => (
+                (<option key={item.name} value={item.name}>{item.name}</option>)
+            ));
+        }
+
         return (
             <div>
                 <label htmlFor="factoryId" className="form-label">Factory</label>
                 <select className="form-select" id="factoryId" name="factoryId" onChange={this.onFactoryChanged} value={this.state.currentFactory}>
-                    <option value="wsu-test-team-1">Team 1 Factory</option>
-                    <option value="wsu-test-team-8">Team 8 Factory</option>
+                    {factories}
                 </select>
             </div>
         );
