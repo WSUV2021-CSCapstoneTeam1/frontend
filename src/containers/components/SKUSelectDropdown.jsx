cdf872a41693227891d17a8934a93c7b5d30f500
Malcolm Anderson <malcolminyo@gmail.com>
2022-03-01 14:17:04 -0800

    Order form generates JSON, we have SKU select dropdown

---
+++ src/containers/components/SKUSelectDropdown.jsx
@@ -0,0 +1,68 @@
+import { Component } from 'react';
+
+class SKUSelectDropdown extends Component {
+    /*
+    This dropdown allows a user to select a SKU when they're submitting an order
+    */
+
+    constructor(props) {
+        super(props);
+        this.state = {currentSKU: '', allSKUs: [] };
+        this.onSKUChanged = this.onSKUChanged.bind(this);
+    }
+
+    componentDidMount() {
+        const headers = { 'Content-Type': 'application/json', 'Accept': 'application/json', 'siteflow-organization': 'wsu-test-team-1' }
+        fetch('http://54.191.60.209:8090/BackendApi-1.0-SNAPSHOT/api/sku/siteflow/get/all', { mode: 'cors', headers: headers })
+              .then(response => {
+                if (response.ok) {
+                   return response.json()
+               }
+               throw response;
+              })
+                .then(data => {
+                    console.log(data);
+                this.setState({
+                  allSKUs: data
+                })
+              })
+              .catch(error => {
+                console.log(error);
+              })
+    }
+
+    onSKUChanged(event) {
+        var newSKU = event.target.value;
+        this.setState({ currentSKU: newSKU });
+        if (this.props.onSKUChanged != null) {
+            this.props.onSKUChanged(newSKU);
+        }
+        else {
+            console.log('SKUSelectDropdown - No onSKUChanged callback supplied');
+        }
+    }
+
+    render() {
+        var skus = [];
+        if (this.state.allSKUs.data == null) {
+            console.log('no SKUs');
+            skus = null;
+        }
+        else {
+            skus = this.state.allSKUs.data.map((item) => (
+                (<option data={item.code} key={item._id}>{item.code}</option>)
+            ));
+        }
+
+        return (
+            <div>
+                <label htmlFor="SKUId" className="form-label">SKU</label>
+                <select className="form-select" id="SKUId" name="SKUId" onChange={this.onSKUChanged} value={this.state.currentSKU}>
+                    {skus}
+                </select>
+            </div>
+        );
+    }
+
+}
+export default SKUSelectDropdown;
--- src/containers/pages/AddSiteFlowOrderView.jsx
+++ src/containers/pages/AddSiteFlowOrderView.jsx
@@ -1,16 +1,32 @@
 import { Component } from 'react';
-import { Redirect } from 'react-router';
-
+// import { Redirect } from 'react-router';
+import FactorySelectDropdown from '../components/FactorySelectDropdown';
+import SKUSelectDropdown from '../components/SKUSelectDropdown';
 import BootstrapAlert from '../components/BootstrapAlert';
 
 class AddSiteOrderSKUView extends Component {
     /*
-    This is the page where a user can create and submit a SKU to Site Flow.
+    This is the page where a user can create and submit an order to Site Flow.
     */
     constructor(props) {
         super(props);
-        this.state = { responseCode: null, mode: 'new', id: -1000, data: {}, products: [] };
+        this.state = {
+            responseCode: null,
+            factory: '',
+            orderID: '',
+            sku: '',
+            shipping: {
+                name: '',
+                companyName: '',
+                address1: '',
+                town: '',
+                postcode: '',
+                isoCountry: ''
+            }
+        };
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.onFactoryChanged = this.onFactoryChanged.bind(this);
+        this.onSKUChanged = this.onSKUChanged.bind(this);
     }
 
     componentDidMount() {
@@ -42,52 +58,74 @@ class AddSiteOrderSKUView extends Component {
 
         // Build the JSON object we want to send to the backend
         var newOrderData = {
-
-            accountName: e.target.accountName.value,
-            orderID: e.target.orderID.value,
-            // array of skus named items?
-
-            // ship to - Make this an array of address objects?
-            shippingName: e.target.shippingName.value,
-            companyName: e.target.companyName.value,
-            address: e.target.address.value,
-            town: e.target.town.value,
-            postCode: e.target.areaCode.value,
-            isoCountry: e.target.isoCountry.value,
-
+            destination: {
+                name: this.state.factory
+            },
+            orderData: {
+                sourceOrderId: this.state.orderID,
+                items: [
+                    {
+                        sku: this.state.sku,
+                        sourceItemId: "SOURCE ITEM ID",
+                        shipmentIndex: 0,
+                        components: {
+                            fetch: true,
+                            code: "SOME CODE",
+                            path: "URL",
+                            sourceComponentId: "SOURCE COMPONENT ID"
+                        }
+                    }
+                ]
+            },
+            shipments: [
+                {
+                    shipmentIndex: 0,
+                    shipTo: this.state.shipping,
+                    carrier: {
+                        code: "fedex",
+                        service: "ground"
+                    }
+                }
+            ]
         };
 
         console.log(JSON.stringify(newOrderData));
-        console.log('Going to make the POST request...');
+        // console.log('Going to make the POST request...');
+        // fetch('http://54.191.60.209:8090/BackendApi-1.0-SNAPSHOT/api/order/siteflow/post', {
+        //     method: 'POST',
+        //     headers: {
+        //         'Content-Type': 'application/json'
+        //     },
+        //     body: JSON.stringify(newOrderData)
+        // })
+        //     .then((response) => {
+        //         console.log(response);
+        //         console.log(`Got response from the POST request with ${response.status}`);
+        //         console.log(`this state is ${this.state}`);
+        //         this.setState({ responseCode: response.status });
+        // });
+    }
 
-        fetch('http://54.191.60.209:8090/BackendApi-1.0-SNAPSHOT/api/sku/siteflow/post', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(newOrderData)
-        })
-            .then((response) => {
-                console.log(response);
-                console.log(`Got response from the POST request with ${response.status}`);
-                console.log(`this state is ${this.state}`);
-                this.setState({ responseCode: response.status });
-        });
+    onFactoryChanged(newFactory) {
+        this.setState({ factory: newFactory });
+    }
+
+    onSKUChanged(newSKU) {
+        this.setState({ sku: newSKU })
+    }
+
+    onShippingNameUpdated(e) {
+        this.setState({ shipping: { name: e.target.value } });
     }
 
     render() {
-
-        let inEditMode = this.state.mode === 'edit';
-
         // Figure out what alert to display
         let alert = null;
         if (this.state.responseCode != null) {
             if (this.state.responseCode === 201) {
-//                 alert = (<Redirect to="/siteflow/sku" />);
-                console.log("Stuff is ok");
+                alert = (<BootstrapAlert alertType='success' content={`Order posted successfully!`} />);
             } else {
-//                 alert = (<BootstrapAlert alertType='danger' content={`SKU ${inEditMode ? 'edit' : 'creation'} failed with code ${this.state.responseCode}`} />);
-                console.log("Stuff went wrong");
+                alert = (<BootstrapAlert alertType='danger' content={`Order failed with code ${this.state.responseCode}`} />);
             }
         }
 
@@ -97,61 +135,68 @@ class AddSiteOrderSKUView extends Component {
                 <p>Create new Order for SiteFlow.</p>
 
                 {/* Account Name */}
-                <form onSubmit={this.handleSubmit} class="needs-validation" noValidate>
+                <form onSubmit={this.handleSubmit} className="needs-validation" noValidate>
                     <div className="mb-3">
-                        <label htmlFor="accountName" className="form-label">Account Name</label>
-                        <input type="text" className="form-control" id="accountName" name="accountName" defaultValue={ inEditMode ? this.state.data.skuCode : ''} required></input>
-                        <div class="invalid-feedback">Please fill out this field.</div>
+                        <FactorySelectDropdown onFactoryChanged={this.onFactoryChanged} />
+                        <div className="invalid-feedback">Please fill out this field.</div>
                     </div>
 
                     {/* Order ID */}
                     <div className="mb-3">
                         <label htmlFor="orderID" className="form-label">Order ID</label>
-                        <input type="text" className="form-control" id="orderID" name="orderID" defaultValue={ inEditMode ? this.state.data.description : ''} required></input>
-                        <div class="invalid-feedback">Please fill out this field.</div>
+                        <input type="text" className="form-control" id="orderID" name="orderID" required></input>
+                        <div className="invalid-feedback">Please fill out this field.</div>
+                    </div>
+
+                    {/* SKU */}
+                    <div className="mb-3">
+                        {/* <label htmlFor="sku" className="form-label">SKU</label>
+                        <input type="text" className="form-control" id="sku" name="sku" value={this.state.sku} onChange={this.onSkuChanged} required></input> */}
+                        <SKUSelectDropdown onSKUChanged={this.onSKUChanged} />
+                        <div className="invalid-feedback">Please fill out this field.</div>
                     </div>
 
                     <h4>Shipping Address</h4>
 
                     {/* Shipping Name */}
                     <div className="mb-3">
-                        <label htmlFor="shippingName" className="form-label">Name</label>
-                        <input type="text" className="form-control" id="shippingName" name="shippingName" defaultValue={ inEditMode ? this.state.data.description : ''} required></input>
-                        <div class="invalid-feedback">Please fill out this field.</div>
+                        <label htmlFor="name" className="form-label">Name</label>
+                        <input type="text" className="form-control" id="name" name="name" value={this.state.shipping.name} onChange={this.onShippingNameUpdated} required></input>
+                        <div className="invalid-feedback">Please fill out this field.</div>
                     </div>
 
                     {/* Company Name */}
                     <div className="mb-3">
                         <label htmlFor="companyName" className="form-label">Company</label>
-                        <input type="text" className="form-control" id="companyName" name="companyName" defaultValue={ inEditMode ? this.state.data.description : ''} required></input>
-                        <div class="invalid-feedback">Please fill out this field.</div>
+                        <input type="text" className="form-control" id="companyName" name="companyName" required></input>
+                        <div className="invalid-feedback">Please fill out this field.</div>
                     </div>
 
                     {/* Address */}
                     <div className="mb-3">
                         <label htmlFor="address" className="form-label">Address</label>
-                        <input type="text" className="form-control" id="address" name="address" defaultValue={ inEditMode ? this.state.data.description : ''} required></input>
-                        <div class="invalid-feedback">Please fill out this field.</div>
+                        <input type="text" className="form-control" id="address" name="address" required></input>
+                        <div className="invalid-feedback">Please fill out this field.</div>
                     </div>
 
                     {/* Town */}
                     <div className="mb-3">
                         <label htmlFor="town" className="form-label">Town</label>
-                        <input type="text" className="form-control" id="town" name="town" defaultValue={ inEditMode ? this.state.data.description : ''} required></input>
-                        <div class="invalid-feedback">Please fill out this field.</div>
+                        <input type="text" className="form-control" id="town" name="town" required></input>
+                        <div className="invalid-feedback">Please fill out this field.</div>
                     </div>
 
                     {/* Area Code */}
                     <div className="mb-3">
                         <label htmlFor="areaCode" className="form-label">Area Code</label>
-                        <input type="text" className="form-control" id="areaCode" name="areaCode" defaultValue={ inEditMode ? this.state.data.description : ''} required></input>
-                        <div class="invalid-feedback">Please fill out this field.</div>
+                        <input type="text" className="form-control" id="areaCode" name="areaCode" required></input>
+                        <div className="invalid-feedback">Please fill out this field.</div>
                     </div>
 
                     {/* Country iso */}
                     <div className="mb-3">
-                        <label htmlFor="type" className="form-label">Country</label>
-                        <select className="form-select" id="type" name="type" defaultValue={ inEditMode ? this.state.data.type : 'html'}>
+                        <label htmlFor="isoCountry" className="form-label">Country</label>
+                        <select className="form-select" id="isoCountry" name="isoCountry">
                             <option value="US">US</option>
                             <option value="PE">PE</option>
                             <option value="MA">MA</option>
